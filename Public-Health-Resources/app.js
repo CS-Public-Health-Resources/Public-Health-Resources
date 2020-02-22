3a2b41ab1cdc8d3a8489c8a821791465e04d2d90
James <jsasser143801@gmail.com>
2020-02-22 15:52:23 -0600

    added server app

---
+++ Public-Health-Resources/app.js
@@ -0,0 +1,15 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+    fs.readFile('Public_Health_Resources.html' , function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+  }).listen(8000);
