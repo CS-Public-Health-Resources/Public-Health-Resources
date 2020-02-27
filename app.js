1e697d57222c60a44363a358bf0ee07fdd568706
James <jsasser143801@gmail.com>
2020-02-27 10:13:16 -0600

    Added certification

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var https = require('https');
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var credentials = {key: privateKey, cert:certificate};
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +20,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(8000);
 
 module.exports = app;
